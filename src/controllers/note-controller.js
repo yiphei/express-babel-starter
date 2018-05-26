5432a853342d9e0b3f4d6f79af39a31016ee87cc
yiphei <yanyiphei@gmail.com>
2018-05-26 16:16:54 -0400

    m

---
+++ src/controllers/note-controller.js
@@ -0,0 +1,41 @@
+import Note from '../models/note-model';
+
+
+export const getNotes = () => {
+  return Note.find({}).then((notes) => {
+    return notes.reduce((result, item) => {
+      result[item.id] = item;
+      return result;
+    }, {});
+  });
+};
+
+export const deleteNote = (id) => {
+  // to quote Prof. Cormen: left as an exercise to the reader
+  // remember to return the mongoose function you use rather than just delete
+  return Note.remove({ _id: id });
+};
+
+export const createNote = (fields) => {
+  // you know the drill. create a new Note mongoose object
+  // return .save()
+
+  const note = new Note();
+  note.title = fields.title;
+  note.x = fields.x;
+  note.y = fields.y;
+  note.zIndex = fields.zIndex;
+  note.text = fields.text;
+  return note.save();
+};
+
+export const updateNote = (id, fields) => {
+  return Note.findById(id)
+    .then((note) => {
+    // check out this classy way of updating only the fields necessary
+      Object.keys(fields).forEach((k) => {
+        note[k] = fields[k];
+      });
+      return note.save();
+    });
+};
--- src/server.js
+++ src/server.js
@@ -3,9 +3,24 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import socketio from 'socket.io';
+import http from 'http';
+import * as Notes from './controllers/note-controller';
+
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/notes';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
 
 // initialize
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
@@ -35,6 +50,50 @@ app.get('/', (req, res) => {
 // START THE SERVER
 // =============================================================================
 const port = process.env.PORT || 9090;
-app.listen(port);
+server.listen(port);
 
 console.log(`listening on: ${port}`);
+
+
+// at the bottom of server.js
+// lets register a connection listener
+io.on('connection', (socket) => {
+  // on first connection emit notes
+  Notes.getNotes().then((result) => {
+    socket.emit('notes', result);
+  });
+
+  // pushes notes to everybody
+  const pushNotes = () => {
+    Notes.getNotes().then((result) => {
+      // broadcasts to all sockets including ourselves
+      io.sockets.emit('notes', result);
+    });
+  };
+
+  // creates notes and
+  socket.on('createNote', (fields) => {
+    Notes.createNote(fields).then((result) => {
+      pushNotes();
+    }).catch((error) => {
+      console.log(error);
+      socket.emit('error', 'create failed');
+    });
+  });
+
+  // on update note do what is needful
+  socket.on('updateNote', (id, fields) => {
+    Notes.updateNote(id, fields).then(() => {
+      pushNotes();
+    });
+  });
+
+
+  // on deleteNote do what is needful
+  socket.on('deleteNote', (id) => {
+  // you can do it
+    Notes.deleteNote(id).then(() => {
+      pushNotes();
+    });
+  });
+});
